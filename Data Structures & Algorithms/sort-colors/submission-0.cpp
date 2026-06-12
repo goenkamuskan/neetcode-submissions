3cf4032f87dec10ed2fc93004e13ae41ceba1ca7
Muskan Goenka <153921001+goenkamuskan@users.noreply.github.com>
2026-06-12 13:12:11 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int count_0=0;
+        int count_1=0;
+        int count_2=0;
+        for(int i=0;i<n;i++){
+            if (nums[i]==0) count_0++;
+            else if( nums[i]==1) count_1 ++;
+            else count_2++;
+        }
+        int idx=0;
+        for(int i=0;i<count_0;i++){
+            nums[idx]=0;
+            idx++;
+        }
+        for(int i=0;i<count_1;i++){
+            nums[idx]=1;
+            idx++;
+        }
+        for(int i=0;i<count_2;i++){
+            nums[idx]=2;
+            idx++;
+        }
+    }
+};
